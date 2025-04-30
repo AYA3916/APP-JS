261f676ce66981465e672e4c26cef465abbdecf4
L'ASSRI Meriem <meriem.lassri@etu.uae.ac.ma>
2025-04-30 14:15:16 +0100

    log out

--- dashboards/espaceprof/espace-prof.js
+++ dashboards/espaceprof/espace-prof.js
@@ -4,15 +4,3 @@ const detailsBtn = document.querySelector(".sidebar-container .details-btn");
 detailsBtn.addEventListener("click", () => {
   sidebarContainer.classList.toggle("active");
 });
-
-document.getElementById("logoutBtn").addEventListener("click", function (e) {
-  e.preventDefault();
-
-  const confirmLogout = confirm("Êtes-vous sûr de vouloir vous déconnecter ?");
-
-  if (confirmLogout) {
-    localStorage.clear();
-    window.location.href = "/APP-JS/LOGIN/login.html";
-
-  }
-});
